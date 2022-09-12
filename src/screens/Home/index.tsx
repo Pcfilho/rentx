e0f7816f28ddbe914f928bad57adf88b20614eca
paulocesar <paulofilhobarroso@gmail.com>
2022-09-12 17:34:56 -0300

    Criação do Header da Home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
-
+import { RFValue } from 'react-native-responsive-fontsize';
 import Logo from '../../assets/logo.svg';
 
 import {
   Container,
   Header,
+  TotalCars,
+  HeaderContent
 } from './styles';
 
 export function Home() {
@@ -17,7 +19,15 @@ export function Home() {
         translucent
       />
       <Header>
-
+        <HeaderContent>
+          <Logo 
+            width={RFValue(108)}
+            height={RFValue(12)}
+          />
+          <TotalCars>
+            Total de 12 carros
+          </TotalCars>
+        </HeaderContent>
       </Header>
     </Container>
   );
--- src/screens/Home/styles.tsx
+++ src/screens/Home/styles.tsx
@@ -1,3 +1,4 @@
+import { RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
 
 
@@ -12,4 +13,21 @@ export const Header =  styled.View`
   height: 113px;
 
   background-color: ${({ theme }) => theme.colors.header};
+
+  justify-content: flex-end;
+  padding: 32px 24px;
+`;
+
+export const HeaderContent = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const TotalCars = styled.Text`
+  font-size: ${RFValue(15)}px;
+  font-family: ${({ theme }) => theme.fonts.primary_400};
+
+  color: ${({ theme }) => theme.colors.text};
+  
 `;
