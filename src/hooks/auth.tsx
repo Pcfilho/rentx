ab1b1ac2b8f8ea5dfbd23dbb76aaa3273d639a61
Paulo <paulofilhobarroso@gmail.com>
2023-02-06 16:47:14 -0300

    ImagePicker, Updating User in DB

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,103 +1,144 @@
-import { Model } from '@nozbe/watermelondb';
+import { Model } from "@nozbe/watermelondb";
 import React, {
-    createContext,
-    useState,
-    useContext,
-    ReactNode,
-    useEffect
-} from 'react';
-import { database } from '../database';
-import { User as ModelUser } from '../database/models/User';
+  createContext,
+  useState,
+  useContext,
+  ReactNode,
+  useEffect,
+} from "react";
+import { err } from "react-native-svg/lib/typescript/xml";
+import { database } from "../database";
+import { User as ModelUser } from "../database/models/User";
 
-import { api } from '../services/api';
+import { api } from "../services/api";
 
 interface User {
-    id: string;
-    email: string;
-    name: string;
-    driver_license: string;
-    avatar: string;
-    token: string;
+  id: string;
+  user_id: string;
+  email: string;
+  name: string;
+  driver_license: string;
+  avatar: string;
+  token: string;
 }
 
 interface SignInCredentials {
-    email: string;
-    password: string;
+  email: string;
+  password: string;
 }
 
 interface AuthContextData {
-    user: User;
-    signIn: (credentials: SignInCredentials) => Promise<void>;
+  user: User;
+  signIn: (credentials: SignInCredentials) => Promise<void>;
+  signOut: () => Promise<void>;
+  updateUser: (user: User) => Promise<void>;
 }
 
 interface AuthProviderProps {
-    children: ReactNode;
+  children: ReactNode;
 }
 
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
-const AuthProvider = ({ children } : AuthProviderProps) => {
-    const [data, setData] = useState<User>({} as User);
+const AuthProvider = ({ children }: AuthProviderProps) => {
+  const [data, setData] = useState<User>({} as User);
 
-    const signIn = async ({ email, password }: SignInCredentials) => {
-        try {
-            const response = await api.post('/sessions', {
-                email,
-                password,
-            });
-    
-            const { token, user } = response.data;
-    
-            api.defaults.headers.authorization = `Bearer ${token}`;
+  const signIn = async ({ email, password }: SignInCredentials) => {
+    try {
+      const response = await api.post("/sessions", {
+        email,
+        password,
+      });
 
-            const userCollection = database.get<ModelUser>('users');
-            await database.action(async () => {
-                await userCollection.create(( newUser ) => {
-                        newUser.user_id = user.id,
-                        newUser.name = user.name,
-                        newUser.email = user.email,
-                        newUser.driver_license = user.driver_license,
-                        newUser.avatar = user.avatar,
-                        newUser.token = token
-                })
-            })
-    
-            setData({ ...user, token });
-        } catch (error) {
-            throw new Error(error);
-        }
-        
+      const { token, user } = response.data;
+
+      api.defaults.headers.authorization = `Bearer ${token}`;
+
+      const userCollection = database.get<ModelUser>("users");
+      await database.action(async () => {
+        await userCollection.create((newUser) => {
+          (newUser.user_id = user.id),
+            (newUser.name = user.name),
+            (newUser.email = user.email),
+            (newUser.driver_license = user.driver_license),
+            (newUser.avatar = user.avatar),
+            (newUser.token = token);
+        });
+      });
+
+      setData({ ...user, token });
+    } catch (error) {
+      throw new Error(error);
     }
+  };
 
-    useEffect(() => {
-        async function loadUserData() {
-            const userCollection = database.get<ModelUser>('users');
-            const response = await userCollection.query().fetch();
-            
-            if (response.length > 0) {
-                const userData = response[0]._raw as unknown as User;
-                api.defaults.headers.authorization = `Bearer ${userData.token}`;
-                setData(userData);
-            }
-        }
-        loadUserData();
-    }, []);
+  const signOut = async () => {
+    try {
+      const userCollection = database.get<ModelUser>("users");
+      await database.action(async () => {
+        const userSelected = await userCollection.find(data.id);
+        await userSelected.destroyPermanently();
+      });
 
-    return (
-        <AuthContext.Provider
-            value={{
-                user: data,
-                signIn
-            }}
-        >
-            {children}
-        </AuthContext.Provider>
-    )
-}
+      setData({} as User);
+    } catch (error) {
+      throw new Error(error);
+    }
+  };
 
-const useAuth = () : AuthContextData  => {
-    const context = useContext(AuthContext);
-    return context;
-} 
+  const updateUser = async (user: User) => { 
+    console.log(user);
+    try {
+      const userCollection = database.get<ModelUser>("users");
+      await database.action(async () => {
+        const userSelected = await userCollection.find(user.id);
+        await userSelected.update((userData) => {
+          userData.name = user.name;
+          userData.driver_license = user.driver_license;
+          userData.avatar = user.avatar;
+        });
+      });
+
+      setData(user);
+      console.log('Finished updating user');
+    } catch (error) {
+      console.log('Update error:', error);
+      throw new Error(error);
+      
+    }
+  };
+
+  useEffect(() => {
+    async function loadUserData() {
+      const userCollection = database.get<ModelUser>("users");
+      const response = await userCollection.query().fetch();
+
+      if (response.length > 0) {
+        const userData = response[0]._raw as unknown as User;
+        api.defaults.headers.authorization = `Bearer ${userData.token}`;
+        setData(userData);
+      }
+    }
+    loadUserData();
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user: data,
+        signIn,
+        signOut,
+        updateUser
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+const useAuth = (): AuthContextData => {
+  const context = useContext(AuthContext);
+  return context;
+};
 
 export { AuthProvider, useAuth };
--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -4,6 +4,8 @@ import {
   TouchableWithoutFeedback,
   Keyboard,
 } from "react-native";
+import * as Yup from "yup";
+import * as ImagePicker from "expo-image-picker";
 import { BackButton } from "../../components/BackButton";
 import { useTheme } from "styled-components";
 import { Feather } from "@expo/vector-icons";
@@ -27,15 +29,19 @@ import {
 import { Input } from "../../components/Input";
 import { PasswordInput } from "../../components/PasswordInput";
 import { useAuth } from "../../hooks/auth";
+import { Button } from "../../components/Button";
 
 type optionType = "data" | "password";
 
 const Profile = () => {
   const theme = useTheme();
-
-  const { user } = useAuth();
+  const { user, signOut, updateUser } = useAuth();
   const { goBack } = useNavigate();
+
   const [option, setOption] = useState<optionType>("data");
+  const [avatar, setAvatar] = useState(user.avatar);
+  const [name, setName] = useState(user.name);
+  const [driverLicense, setDriverLicense] = useState(user.driver_license);
 
   const isDataEdit = () => option === "data";
   const isPasswordEdit = () => option === "password";
@@ -45,7 +51,56 @@ const Profile = () => {
     setOption(optionSelected);
   };
 
-  const handleSignOut = () => {};
+  const handleSelectAvatar = async () => {
+    try {
+
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 4],
+      quality: 1,
+    });
+
+
+    if (result.canceled) {
+      return;
+    }
+
+    if (result.assets[0].uri) {
+      setAvatar(result.assets[0].uri);
+    }
+  } catch(error) {
+    console.log(error)
+  }
+  };
+
+  const handleProfileUpdate = async () => {
+    try {
+      const schema = Yup.object().shape({
+        driverLicense: Yup.string().required('CNH é obrigatória'),
+        name: Yup.string().required('Nome é obrigatório'),
+      })
+
+      const data = { name, driverLicense };
+      await schema.validate(data);
+
+      await updateUser({
+        id: user.id,
+        user_id: user.user_id,
+        email: user.email,
+        avatar,
+        name,
+        driver_license: driverLicense,
+        token: user.token,
+      })
+    } catch (error) {
+      if(error instanceof Yup.ValidationError) {
+        console.log("YUP: " + error.message);
+      } else {
+        console.log("Não foi possível atualizar");
+      }
+    }
+  };
 
   return (
     <KeyboardAvoidingView behavior="position" enabled style={{ flex: 1 }}>
@@ -55,14 +110,14 @@ const Profile = () => {
             <HeaderTop>
               <BackButton color={theme.colors.shape} onPress={goBack} />
               <HeaderTitle>Editar Perfil</HeaderTitle>
-              <LogoutButton onPress={handleSignOut}>
+              <LogoutButton onPress={signOut}>
                 <Feather name="power" size={24} color={theme.colors.shape} />
               </LogoutButton>
             </HeaderTop>
 
             <PhotoContainer>
-              <Photo source={{ uri: "https://github.com/Pcfilho.png" }} />
-              <PhotoButton onPress={() => {}}>
+              { !!avatar && <Photo source={{ uri: avatar }} />}
+              <PhotoButton onPress={handleSelectAvatar}>
                 <Feather name="camera" size={24} color={theme.colors.shape} />
               </PhotoButton>
             </PhotoContainer>
@@ -89,15 +144,28 @@ const Profile = () => {
 
             {isDataEdit() ? (
               <Section>
-                <Input iconName="user" placeholder="nome" autoCorrect={false} defaultValue={user.name} />
+                <Input
+                  iconName="user"
+                  placeholder="nome"
+                  autoCorrect={false}
+                  defaultValue={user.name}
+                  onChangeText={setName}
+                  value={name}
+                />
 
-                <Input iconName="mail" editable={false} defaultValue={user.email} />
+                <Input
+                  iconName="mail"
+                  editable={false}
+                  defaultValue={user.email}
+                />
 
                 <Input
                   iconName="credit-card"
                   placeholder="CNH"
                   keyboardType="numeric"
                   defaultValue={user.driver_license}
+                  onChangeText={setDriverLicense}
+                  value={driverLicense}
                 />
               </Section>
             ) : (
@@ -112,6 +180,11 @@ const Profile = () => {
                 />
               </Section>
             )}
+
+            <Button 
+              title="Salvar alterações"
+              onPress={handleProfileUpdate}
+            />
           </Content>
         </Container>
       </TouchableWithoutFeedback>
